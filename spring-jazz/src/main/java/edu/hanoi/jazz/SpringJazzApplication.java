446783ac617290926d072ca9f005ae5820a30513
Daipa <daipaqn93@gmail.com>
2017-12-05 15:16:36 +0700

    commit first unit7.security.hibernate

--- spring-jazz/src/main/java/edu/hanoi/jazz/SpringJazzApplication.java
+++ spring-jazz/src/main/java/edu/hanoi/jazz/SpringJazzApplication.java
@@ -2,8 +2,10 @@ package edu.hanoi.jazz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config.xml")
 public class SpringJazzApplication {
 
 	public static void main(String[] args) {
